4a8450e275aaf8a2f6868f60fae0c67a34c768b3
natimati <natmateusiak@gmail.com>
2022-07-14 12:50:14 +0200

    adding react hook forms and fixing contact forms styles

--- src/components/ContactForm/index.tsx
+++ src/components/ContactForm/index.tsx
@@ -1,7 +1,13 @@
 import React, { useState } from 'react';
 import emailjs from 'emailjs-com'
-import { Container, EmailFormInput, FormInput, InputContainer, Textarea } from './style';
+import { EmailFormInput, Form, FormContainer, FormInput, InputContainer, InputError, Textarea } from './style';
 import { Button } from '../../styles';
+import { useForm } from 'react-hook-form';
+
+type FormValues = {
+  name: string;
+  email: string;
+};
 
 const Contact = () => {
     const [name, setName] = useState('');
@@ -28,20 +34,49 @@ const Contact = () => {
             setMessage('');
             setEmailSent(true);
         } else {
-            alert('Please fill in all fields')
+            // alert('Please fill in all fields')
         }
     }
 
+    const { register, handleSubmit, formState: { errors } } = useForm<FormValues>();
+    const onSubmit = handleSubmit((data) => console.log(data));
+
     return (
-        <Container>
-            <InputContainer>
-                <FormInput type='text' placeholder='Your Name' value={name} onChange={e => setName(e.target.value)} /> 
-                <EmailFormInput type='email' placeholder='Your email adress' value={email} onChange={e => setEmail(e.target.value)} />
-            </InputContainer>
-            <Textarea placeholder='Your message' value={message} onChange={e => setMessage(e.target.value)} />
-            <Button onClick={submit}>send</Button>
-            <span className={emailSent ? 'visible' : undefined}>Thank you for your message, we will be in touch</span>
-        </Container>
+            <Form onSubmit={onSubmit}>
+                <FormContainer>
+                    <InputContainer>
+                        <FormInput
+                            type='text'
+                            {...register("name", { required: true, minLength: 3, maxLength: 15 })}
+                            placeholder='Your Name'
+                            value={name}
+                            onChange={e => setName(e.target.value)} /> 
+                        <InputError>
+                            {errors.name?.type === "required" && "Please enter your name"}
+                            {errors.name?.type === "minLength" && "Use at least 3 characters"}
+                            {errors.name?.type === "maxLength" && "You can use 20 characters at most"}
+                        </InputError>
+                    </InputContainer>
+                    <InputContainer>
+                        <EmailFormInput
+                            type='email'
+                            {...register("email", {  required: true,
+                                pattern: {
+                                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                    message: "invalid email address",
+                                }})}
+                            placeholder='Your email adress'
+                            value={email}
+                            onChange={e => setEmail(e.target.value)} />
+                        <InputError>
+                            {errors.email?.type === "required" && "Please enter your email"}
+                        </InputError>
+                        </InputContainer>
+                    </FormContainer>
+                <Textarea placeholder='Your message' value={message} onChange={e => setMessage(e.target.value)} />
+                <Button onClick={submit}>send</Button>
+                <span className={emailSent ? 'visible' : undefined}>Thank you for your message, we will be in touch</span>
+            </Form>
     )
 }
 
--- src/components/ContactForm/style.ts
+++ src/components/ContactForm/style.ts
@@ -1,25 +1,34 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
+export const Form = styled.form`
 display: flex;
-flex-direction: column; 
+flex-direction: column;
+margin: 20px;
 gap: 20px;
-margin: 50px;
+max-width: 900px;
+`;
+
+export const FormContainer = styled.form`
+display: flex;
+flex-direction: row;
+gap: 20px;
+max-width: 900px;
 `;
 
 export const InputContainer = styled.div`
 display: flex;
-flex-direction: row;
-gap: 40px;
+flex-direction: column;
+max-width: 100%;
 `;
 
 export const FormInput = styled.input`
 height: 30px;
-width: 40%;
 font-family: ${(props) => props.theme.fontFamily.primaryFont};
 font-size: 16px;
 border: 2px solid ${(props) => props.theme.colors.primaryUi};
 padding: 5px;
+min-width: 30%;
+width: 200px;
 
 &:focus {
     outline: none;
@@ -28,8 +37,15 @@ padding: 5px;
 `;
 
 export const EmailFormInput = styled(FormInput)`
-width: 60%;
+min-width: 60%;
+width: 650px;
 `;
+
+export const InputError = styled.span`
+align-self: flex-start;
+padding: 3px;
+font-size: 14px;
+`
 
 export const Textarea = styled.textarea`
 height: 100px;

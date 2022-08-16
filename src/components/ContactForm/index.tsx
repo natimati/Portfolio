2deec9a48e4a287679dfeb559994835bb2094557
natimati <natmateusiak@gmail.com>
2022-08-16 10:39:31 +0200

    fixing contact form inputs parameters

--- src/components/ContactForm/index.tsx
+++ src/components/ContactForm/index.tsx
@@ -47,68 +47,92 @@ const Contact = () => {
     }
     
     return (
-        <Container>
-            <h1>Want to connect? Let's talk!</h1>
-            <Form onSubmit={onSubmit}>
-                <NameInputContainer>
-                    <Label>Name</Label>
-                    <FormInput
-                        type='text'
-                        {...register("name", { required: true, minLength: 3, maxLength: 15 })}
-                        placeholder='Your name'
-                    /> 
-                    <InputError>
-                        {errors.name?.type === "required" && "Please enter your name"}
-                        {errors.name?.type === "minLength" && "Use at least 3 characters"}
-                        {errors.name?.type === "maxLength" && "You can use 20 characters at most"}
-                    </InputError>
-                </NameInputContainer>
-                <EmailInputContainer>
-                    <Label>E-mail</Label>
-                    <FormInput
-                        type='email'
-                        {...register("email", {  required: true, minLength: 3, maxLength: 320,
-                            pattern: {
-                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                                message: "invalid email address",
-                        }})}
-                        placeholder='Your email adress' />
-                    <InputError>
-                        {errors.email?.type === "required" && "Please enter your email"}
-                        {errors.email?.type === "minLength" && "Use at least 3 characters"}
-                        {errors.email?.type === "maxLength" && "You can use 320 characters at most"}
-                    </InputError>
-                </EmailInputContainer>
-                <MessageInputContainer>
-                    <Label>Message</Label>
-                    <Message
-                        {...register("message", {  required: true, minLength: 10, maxLength: 320 })}
-                        placeholder='Your message'
-                    />
-                    <InputError>
-                        {errors.message?.type === "required" && "Please enter your message"}
-                        {errors.message?.type === "minLength" && "Use at least 10 characters"}
-                        {errors.message?.type === "maxLength" && "You can use 2000 characters at most"}
-                    </InputError>
-                </MessageInputContainer>
-                <ReCAPTCHAContainer>
-                    <ReCAPTCHA
-                        sitekey={process.env.REACT_APP_SITE_KEY as string}
-                        ref={ref => captchaRef.current = ref}
-                        onChange={onChange} />
-                </ReCAPTCHAContainer>
-                <SendButton type="submit">send</SendButton>
-            </Form>
-            <RodoInformation>
-                    Personal data contained in the above form will be processed by the authors of the website
-                    https://bnportfolio.netlify.app (e-mail: beti.i.naska@gmail.com). <br />Personal data provided via the
-                    contact form will be processed only for the purpose of responding to your message. <br />You have
-                    the right to withdraw consent at any time and request access, rectification, deletion
-                    and limitation of your data. Providing personal data is voluntary, however, without providing it,
-                    we will not be able to respond to the message.
-            </RodoInformation>
-        </Container>
-    )
+      <Container>
+        <h1>Want to connect? Let's talk!</h1>
+        <Form onSubmit={onSubmit}>
+          <NameInputContainer>
+            <Label>Name</Label>
+            <FormInput
+              type="text"
+              {...register("name", {
+                required: true,
+                minLength: 3,
+                maxLength: 50,
+              })}
+              placeholder="Your name"
+            />
+            <InputError>
+              {errors.name?.type === "required" && "Please enter your name"}
+              {errors.name?.type === "minLength" && "Use at least 3 characters"}
+              {errors.name?.type === "maxLength" &&
+                "You can use 50 characters at most"}
+            </InputError>
+          </NameInputContainer>
+          <EmailInputContainer>
+            <Label>E-mail</Label>
+            <FormInput
+              type="email"
+              {...register("email", {
+                required: true,
+                minLength: 6,
+                maxLength: 320,
+                pattern: {
+                  value:
+                    /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                  message: "invalid email address",
+                },
+              })}
+              placeholder="Your email adress"
+            />
+            <InputError>
+              {errors.email?.type === "required" && "Please enter your email"}
+              {errors.email?.type === "minLength" &&
+                "Use at least 6 characters"}
+              {errors.email?.type === "maxLength" &&
+                "You can use 320 characters at most"}
+            </InputError>
+          </EmailInputContainer>
+          <MessageInputContainer>
+            <Label>Message</Label>
+            <Message
+              {...register("message", {
+                required: true,
+                minLength: 10,
+                maxLength: 2000,
+              })}
+              placeholder="Your message"
+            />
+            <InputError>
+              {errors.message?.type === "required" &&
+                "Please enter your message"}
+              {errors.message?.type === "minLength" &&
+                "Use at least 10 characters"}
+              {errors.message?.type === "maxLength" &&
+                "You can use 2000 characters at most"}
+            </InputError>
+          </MessageInputContainer>
+          <ReCAPTCHAContainer>
+            <ReCAPTCHA
+              sitekey={process.env.REACT_APP_SITE_KEY as string}
+              ref={(ref) => (captchaRef.current = ref)}
+              onChange={onChange}
+            />
+          </ReCAPTCHAContainer>
+          <SendButton type="submit">send</SendButton>
+        </Form>
+        <RodoInformation>
+          Personal data contained in the above form will be processed by the
+          authors of the website https://bnportfolio.netlify.app (e-mail:
+          beti.i.naska@gmail.com). <br />
+          Personal data provided via the contact form will be processed only for
+          the purpose of responding to your message. <br />
+          You have the right to withdraw consent at any time and request access,
+          rectification, deletion and limitation of your data. Providing
+          personal data is voluntary, however, without providing it, we will not
+          be able to respond to the message.
+        </RodoInformation>
+      </Container>
+    );
 }
 
 export default Contact;
